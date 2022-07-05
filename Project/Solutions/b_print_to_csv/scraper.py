79e20314f93567dafe83518c7569b742a4423969
ariannedee <ariannedee@gmail.com>
2022-07-05 12:52:07 -0700

    Update to Jul 5 class solutions

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -1,7 +1,6 @@
-import csv
 import requests
 from bs4 import BeautifulSoup
-
+from csv import DictWriter
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
@@ -14,30 +13,31 @@ assert name and email
 headers = {'User-Agent': f'{name} ({email})'}
 response = requests.get(URL, headers=headers)
 
-response.raise_for_status()
+assert response.status_code == 200, f"Got {response.status_code}: {response.reason}"
 
 html_doc = response.text
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-table = soup.find('table', attrs={'class': 'wikitable'})
+table = soup.find('table', class_='wikitable')
 
-countries = []
-for row in table.find_all('tr'):
-    tds = row.find_all('td')
-    if len(tds) == 0:
-        continue
-    name_link = tds[0].a
-    name = name_link['title']
-    date_joined = tds[1].text.strip()
+country_dicts = []
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    if len(cols) > 0:
+        name_col = cols[0]
+        name_str: str = name_col.text
+        name = name_str.split('[')[0].strip()
+        date_joined = cols[1].text.strip()
+        country_dict = {
+            'Name': name,
+            'Date Joined': date_joined
+        }
+        country_dicts.append(country_dict)
 
-    country_dict = {
-        'Name': name,
-        'Date Joined': date_joined,
-    }
-    countries.append(country_dict)
+print(country_dicts)
 
 with open('data/countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, ['Name', 'Date Joined'])
+    writer = DictWriter(file, fieldnames=['Name', 'Date Joined'])
     writer.writeheader()
-    writer.writerows(countries)
+    writer.writerows(country_dicts)
