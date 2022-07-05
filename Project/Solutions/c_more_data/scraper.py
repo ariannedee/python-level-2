e357d079155f11291bbaed3c9819d9fc70387c34
ariannedee <ariannedee@gmail.com>
2022-07-05 13:09:23 -0700

    Update to Jul 5 class solutions

--- Project/Solutions/c_more_data/scraper.py
+++ Project/Solutions/c_more_data/scraper.py
@@ -1,64 +1,60 @@
-import csv
 import time
 
 import requests
 from bs4 import BeautifulSoup
+from csv import DictWriter
 
 BASE_URL = "https://en.wikipedia.org"
-URL = f"{BASE_URL}/wiki/Member_states_of_the_United_Nations"
+URL = BASE_URL + "/wiki/Member_states_of_the_United_Nations"
 
 # Todo: Update with your info
 name = None
 email = None
 assert name and email
 
-# Dictionary of HTTP headers
 headers = {'User-Agent': f'{name} ({email})'}
 
 response = requests.get(URL, headers=headers)
 
-response.raise_for_status()
+assert response.status_code == 200, f"Got {response.status_code}: {response.reason}"
 
 html_doc = response.text
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-table = soup.find('table', attrs={'class': 'wikitable'})
+table = soup.find('table', class_='wikitable')
 
-countries = []
-for row in table.find_all('tr'):
-    tds = row.find_all('td')
-    if len(tds) == 0:
-        continue
-    name_link = tds[0].a
-    url = BASE_URL + name_link['href']
-    name = name_link['title']
-    date_joined = tds[1].text.strip()
+country_dicts = []
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    if len(cols) > 0:
+        name_col = cols[0]
+        name_str: str = name_col.text
+        name = name_str.split('[')[0].strip()
+        date_joined = cols[1].text.strip()
+        url = name_col.a['href']
+        country_dict = {
+            'Name': name,
+            'Date Joined': date_joined,
+            'URL': BASE_URL + url,
+        }
+        country_dicts.append(country_dict)
 
-    country_dict = {
-        'Name': name,
-        'Date Joined': date_joined,
-        'URL': url,
-    }
-    countries.append(country_dict)
+for country_dict in country_dicts[:10]:
+    response = requests.get(country_dict['URL'], headers=headers)  # Get the URL from the dictionary
 
-print(countries)
+    if response.status_code != 200:
+        print(f"Error getting page for {country_dict['Name']}: {country_dict['URL']}")
 
-for country in countries[:3]:
-    url = country['URL']
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-
-    lat = soup.find('span', class_='latitude').text
-    lon = soup.find('span', class_='longitude').text
-    country['Latitude'] = lat
-    country['Longitude'] = lon
-
-    time.sleep(0.5)
+    lat = soup.find('span', class_='latitude').string
+    lon = soup.find('span', class_='longitude').string
+    country_dict['Latitude'] = lat
+    country_dict['Longitude'] = lon
+    time.sleep(0.5)  # Pause to avoid too frequent requests
 
 with open('data/countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, ['Name', 'Date Joined', 'Latitude', 'Longitude'], extrasaction='ignore')
+    writer = DictWriter(file, fieldnames=['Name', 'Date Joined', 'URL', 'Latitude', 'Longitude'], extrasaction='ignore')
     writer.writeheader()
-    writer.writerows(countries)
+    writer.writerows(country_dicts)
