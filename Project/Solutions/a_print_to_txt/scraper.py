33905eab082c830bcc671cebfda5ef5420fe59ec
ariannedee <ariannedee@gmail.com>
2022-09-21 12:48:36 -0700

    Update to Sep 21 class solutions

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -2,34 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
+DOWNLOAD = False
 
 # Todo: Update with your info
 name = None
 email = None
 assert name and email
 
-# Dictionary of HTTP headers
 headers = {'User-Agent': f'{name} ({email})'}
-response = requests.get(URL, headers=headers)
 
-assert response.status_code == 200, f"Got {response.status_code}: {response.reason}"
+if DOWNLOAD:
+    response = requests.get(URL, headers=headers)
+    response.raise_for_status()
+    html_doc = response.text
+    with open('data/wikipedia.html', 'w', encoding="utf-8") as file:
+        file.write(response.text)
+else:
+    with open('data/wikipedia.html', 'r', encoding="utf-8") as file:
+        html_doc = file.read()
 
-html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-
 table = soup.find('table', class_='wikitable')
-
-countries = []
 rows = table.find_all('tr')
+country_names = []
 for row in rows:
-    cols = row.find_all('td')
-    if len(cols) > 0:
-        name_col = cols[0]
-        name_str: str = name_col.text
-        name = name_str.split('[')[0].strip()
-        countries.append(name)
+    name_col = row.find('th', scope='row')
+    if name_col:
+        link = name_col.a
+        name = link.string
+        country_names.append(name)
 
+print(country_names)
 with open('data/countries.txt', 'w') as file:
-    for country in countries:
-        file.write(country)
+    for name in country_names:
+        file.write(name)
         file.write('\n')
