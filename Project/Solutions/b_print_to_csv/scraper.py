f560d4d57451f0e88eba86daf20a19742fd77f81
ariannedee <ariannedee@gmail.com>
2022-09-21 12:56:17 -0700

    Update to Sep 21 class solutions

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -1,43 +1,43 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-from csv import DictWriter
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
+DOWNLOAD = False
 
 # Todo: Update with your info
 name = None
 email = None
 assert name and email
-
-# Dictionary of HTTP headers
 headers = {'User-Agent': f'{name} ({email})'}
-response = requests.get(URL, headers=headers)
 
-assert response.status_code == 200, f"Got {response.status_code}: {response.reason}"
+if DOWNLOAD:
+    response = requests.get(URL, headers=headers)
+    response.raise_for_status()
+    html_doc = response.text
+    with open('data/wikipedia.html', 'w', encoding="utf-8") as file:
+        file.write(response.text)
+else:
+    with open('data/wikipedia.html', 'r', encoding="utf-8") as file:
+        html_doc = file.read()
 
-html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-
 table = soup.find('table', class_='wikitable')
-
-country_dicts = []
 rows = table.find_all('tr')
+countries = []
 for row in rows:
-    cols = row.find_all('td')
-    if len(cols) > 0:
-        name_col = cols[0]
-        name_str: str = name_col.text
-        name = name_str.split('[')[0].strip()
-        date_joined = cols[1].text.strip()
-        country_dict = {
+    name_col = row.find('th', scope='row')
+    if name_col:
+        link = name_col.a
+        name = link.string
+        date_joined = row.td.text.strip()
+        country = {
             'Name': name,
-            'Date Joined': date_joined
+            'Date Joined': date_joined,
         }
-        country_dicts.append(country_dict)
-
-print(country_dicts)
+        countries.append(country)
 
 with open('data/countries.csv', 'w') as file:
-    writer = DictWriter(file, fieldnames=['Name', 'Date Joined'])
+    writer = csv.DictWriter(file, fieldnames=('Name', 'Date Joined'))
     writer.writeheader()
-    writer.writerows(country_dicts)
+    writer.writerows(countries)
