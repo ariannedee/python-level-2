014e9dbec76ed93ce72905ad68b2e1d8e89762dd
ariannedee <ariannedee@gmail.com>
2022-11-22 12:46:56 -0800

    Update to Nov 22 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -3,41 +3,32 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
-DOWNLOAD = False
 
 # Todo: Update with your info
 name = None
 email = None
 assert name and email
+
 headers = {'User-Agent': f'{name} ({email})'}
+response = requests.get(URL, headers=headers)
 
-if DOWNLOAD:
-    response = requests.get(URL, headers=headers)
-    response.raise_for_status()
-    html_doc = response.text
-    with open('data/wikipedia.html', 'w', encoding="utf-8") as file:
-        file.write(response.text)
-else:
-    with open('data/wikipedia.html', 'r', encoding="utf-8") as file:
-        html_doc = file.read()
+response.raise_for_status()
 
+html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
-countries = []
-for row in rows:
-    name_col = row.find('th', scope='row')
-    if name_col:
-        link = name_col.a
-        name = link.string
-        date_joined = row.td.text.strip()
-        country = {
-            'Name': name,
-            'Date Joined': date_joined,
-        }
-        countries.append(country)
 
-with open('data/countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=('Name', 'Date Joined'))
+table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')[1:]
+country_dicts = []
+for row in rows:
+    name = row.th.a.string
+    date_joined = row.td.span.string
+    country_dict = {'Name': name, 'Date Joined': date_joined}
+    country_dicts.append(country_dict)
+
+print(country_dicts)
+
+with open("countries.csv", "w") as file:
+    writer = csv.DictWriter(file, fieldnames=['Name', 'Date Joined'])
     writer.writeheader()
-    writer.writerows(countries)
+    writer.writerows(country_dicts)
