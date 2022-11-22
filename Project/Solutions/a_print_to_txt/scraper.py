b6fe29212f9bf6cc59de1e5f627fb28582d0d46c
ariannedee <ariannedee@gmail.com>
2022-11-22 12:38:15 -0800

    Update to Nov 22 class

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
-DOWNLOAD = False
 
 # Todo: Update with your info
 name = None
@@ -10,30 +9,23 @@ email = None
 assert name and email
 
 headers = {'User-Agent': f'{name} ({email})'}
+response = requests.get(URL, headers=headers)
 
-if DOWNLOAD:
-    response = requests.get(URL, headers=headers)
-    response.raise_for_status()
-    html_doc = response.text
-    with open('data/wikipedia.html', 'w', encoding="utf-8") as file:
-        file.write(response.text)
-else:
-    with open('data/wikipedia.html', 'r', encoding="utf-8") as file:
-        html_doc = file.read()
+response.raise_for_status()
 
+html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
-country_names = []
-for row in rows:
-    name_col = row.find('th', scope='row')
-    if name_col:
-        link = name_col.a
-        name = link.string
-        country_names.append(name)
 
-print(country_names)
-with open('data/countries.txt', 'w') as file:
-    for name in country_names:
-        file.write(name)
+table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')[1:]
+countries = []
+for row in rows:
+    name = row.th.a.string
+    countries.append(name)
+
+print(len(countries))
+
+with open("countries.txt", "w") as file:
+    for country in countries:
+        file.write(country)
         file.write('\n')
