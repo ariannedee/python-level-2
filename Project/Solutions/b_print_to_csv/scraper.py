04956e4cba1c39f262e50f78aa03ed33eb9c4f0b
ariannedee <ariannedee@gmail.com>
2023-01-19 13:36:36 -0800

    Update for Jan 19 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -1,6 +1,8 @@
 import csv
+
 import requests
 from bs4 import BeautifulSoup
+from requests import Response
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
@@ -10,25 +12,27 @@ email = None
 assert name and email
 
 headers = {'User-Agent': f'{name} ({email})'}
-response = requests.get(URL, headers=headers)
-
+response: Response = requests.get(URL, headers=headers)
 response.raise_for_status()
-
 html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-
 table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')[1:]
-country_dicts = []
-for row in rows:
-    name = row.th.a.string
-    date_joined = row.td.span.string
-    country_dict = {'Name': name, 'Date Joined': date_joined}
-    country_dicts.append(country_dict)
+rows = table.find_all('tr')
 
-print(country_dicts)
+countries = []
+for row in rows[1:]:
+    name_col = row.th
+    link = name_col.a
+    name = link.string
+    date_joined = row.td.span.string.strip()
+    country: dict = {
+        'Name': name,
+        'Date joined': date_joined
+    }
+    countries.append(country)
 
-with open("countries.csv", "w") as file:
-    writer = csv.DictWriter(file, fieldnames=['Name', 'Date Joined'])
+
+with open('countries.csv', 'w') as file:
+    writer = csv.DictWriter(file, ['Name', 'Date joined'])
     writer.writeheader()
-    writer.writerows(country_dicts)
+    writer.writerows(countries)
