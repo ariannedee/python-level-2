2f1514aa0ebdda20471aabf570638f61adc73db2
ariannedee <ariannedee@gmail.com>
2023-04-06 12:42:55 -0700

    Update for Apr 6, 2023 class

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -3,26 +3,31 @@ from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
-# Todo: Update with your info
-name = None
-email = None
-assert name and email
+response = requests.get(URL)
 
-headers = {'User-Agent': f'{name} ({email})'}
-response = requests.get(URL, headers=headers)
 response.raise_for_status()
+
 html_doc = response.text
+
+# with open('UN_countries_full.html', 'w', encoding="utf-8") as file:
+#     file.write(html_doc)
+#
+# with open('UN_countries_full.html', 'r') as file:
+#     html_doc = file.read()
+
 soup = BeautifulSoup(html_doc, 'html.parser')
-table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
 
 countries = []
-for row in rows[1:]:
-    name_col = row.th
-    link = name_col.a
-    name = link.string
+table = soup.find('table', class_='wikitable')
+for row in table.find_all('tr'):
+    col_1 = row.find('th', scope='row')
+    if not col_1:
+        continue
+    name_link = col_1.a
+    name = name_link.string
     countries.append(name)
 
-with open('countries.txt', 'w') as file:
+with open('data/countries.txt', 'w') as file:
     for country in countries:
-        file.write(country + '\n')
+        file.write(country)
+        file.write('\n')
