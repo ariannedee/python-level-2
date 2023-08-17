b85c75e5dcbda2830c3c7f1a9ba8d19a07c17ddc
ariannedee <ariannedee@gmail.com>
2023-08-17 12:52:45 -0700

    Update for Aug 17, 2023 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -1,36 +1,49 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-from csv import DictWriter
+
+
+GET_DATA = False
+WRITE_DATA = True
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
-response = requests.get(URL)
+name = None
+email = None
+headers = {'User-Agent': f'{name} ({email})'}
 
-response.raise_for_status()
+if GET_DATA:
+    response = requests.get(URL, headers=headers)
 
-html_doc = response.text
+    response.raise_for_status()
 
-# with open('UN_countries_full.html', 'w', encoding="utf-8") as file:
-#     file.write(html_doc)
-#
-# with open('UN_countries_full.html', 'r') as file:
-#     html_doc = file.read()
+    with open('wikipedia.html', 'w', encoding="utf-8") as file:
+        html_doc = response.text
+        file.write(html_doc)
+else:
+    with open('wikipedia.html', 'r') as file:
+        html_doc = file.read()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
+table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')
 
 countries = []
-table = soup.find('table', class_='wikitable')
-for row in table.find_all('tr'):
-    col_1 = row.find('th', scope='row')
-    if not col_1:
+for row in rows:
+    name_link = row.th.a
+    if not name_link:  # Header row
         continue
-    name_link = col_1.a
+
     name = name_link.string
-    date_joined = row.td.span.string
-    country = {'name': name, 'date joined': date_joined}
+    date = row.td.span.string
+    country = {
+        'Name': name,
+        'Date Joined': date,
+    }
     countries.append(country)
 
-with open('data/countries.csv', 'w') as file:
-    writer = DictWriter(file, fieldnames=['name', 'date joined'])
-    writer.writeheader()
-    writer.writerows(countries)
+if WRITE_DATA:
+    with open('countries.csv', 'w') as file:
+        writer = csv.DictWriter(file, ['Name', 'Date Joined'])
+        writer.writeheader()
+        writer.writerows(countries)
