5f461c97301606facf22244dd9f1a8f17829d0c0
ariannedee <ariannedee@gmail.com>
2023-10-12 12:21:47 -0700

    Update for Oct 12, 2023 class

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -1,40 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-GET_DATA = False
-
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
 name = None
 email = None
+
+assert name and email, "Fill in your name and email"
+
 headers = {'User-Agent': f'{name} ({email})'}
 
-if GET_DATA:
-    response = requests.get(URL, headers=headers)
+response = requests.get(URL, headers=headers)
+response.raise_for_status()
 
-    response.raise_for_status()
-
-    with open('wikipedia.html', 'w', encoding="utf-8") as file:
-        html_doc = response.text
-        file.write(html_doc)
-else:
-    with open('wikipedia.html', 'r') as file:
-        html_doc = file.read()
-
+html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
-table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
 
 countries = []
-for row in rows:
-    name_link = row.th.a
-    if not name_link:  # Header row
-        continue
 
-    name = name_link.string
-    countries.append(name)
+table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')
+for row in rows:
+    name_column = row.th
+    link = name_column.a
+    if link:
+        country_name = link.string
+        countries.append(country_name)
 
 with open('countries.txt', 'w') as file:
     for country in countries:
-        file.write(country + '\n')
+        file.write(country + "\n")
