e8768a1f2e5bfe1be9c15a2869486224249078b5
ariannedee <ariannedee@gmail.com>
2023-12-12 13:03:44 -0800

    Add Dec 12, 2023 solution

--- Project/Solutions/c_more_data/scraper.py
+++ Project/Solutions/c_more_data/scraper.py
@@ -1,6 +1,4 @@
 import csv
-import logging
-import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,54 +6,51 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://en.wikipedia.org"
 URL = BASE_URL + "/wiki/Member_states_of_the_United_Nations"
 
-name = None
-email = None
-
-assert name and email, "Fill in your name and email"
-
-headers = {'User-Agent': f'{name} ({email})'}
-
-response = requests.get(URL, headers=headers)
+response = requests.get(URL)
 response.raise_for_status()
 
 html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
+table = soup.find('table', class_='wikitable')
+
 countries = []
 
-table = soup.find('table', class_='wikitable')
 rows = table.find_all('tr')
 for row in rows:
-    name_column = row.th
-    link = name_column.a
-    if link:
-        url = link['href']
-        country_name = link.string
-        date_of_admission = row.td.span.string
-        country = {
-            'Name': country_name,
-            'Date of admission': date_of_admission,
-            'URL': BASE_URL + url,
+    name_link = row.th.a
+    if name_link is not None:
+        name = name_link.string
+        date = row.td.span.string
+        country_dict = {
+            'Name': name,
+            'Date of admission': date,
+            'URL': name_link['href']
         }
-        countries.append(country)
+        countries.append(country_dict)
 
-for country in countries[:3]:
-    response = requests.get(country['URL'], headers=headers)
-    if response.status_code != 200:
-        logging.warning(f"Error getting page for {country['Name']} at {country['URL']}")
+print(countries)
+print(len(countries))
+
+for country in countries[0:3]:
+    new_url = BASE_URL + country['URL']
+    response = requests.get(new_url)
+
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        print(f"Error getting request: {new_url}")
+        print(repr(e))
         continue
+
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    try:
-        country['Latitude'] = soup.find('span', class_='latitude').string
-        country['Longitude'] = soup.find('span', class_='longitude').string
-    except AttributeError:
-        logging.warning(f"Error getting lat and lon for {country['Name']} at {country['URL']}")
-    time.sleep(0.5)
 
-print(countries[0])
+    country['Latitude'] = soup.find('span', class_='latitude').string
+    country['Longitude'] = soup.find('span', class_='longitude').string
 
-with open('countries_more.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=('Name', 'Date of admission', 'Latitude', 'Longitude'), extrasaction='ignore')
+
+with open('countries.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=['Name', 'Date of admission', 'Latitude', 'Longitude', 'URL'])
     writer.writeheader()
     writer.writerows(countries)
