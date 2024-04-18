4428af0ebb97d794e818105ef9d61341f4224ad2
ariannedee <ariannedee@gmail.com>
2024-04-18 12:44:50 -0700

    Update solution for Apr 2024 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -1,42 +1,43 @@
 import csv
 import requests
+
 from bs4 import BeautifulSoup
 
 LOAD_DATA = False
-
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
 if LOAD_DATA:
     response = requests.get(URL)
-    print(response.status_code)
     response.raise_for_status()
     html_doc = response.text
 
-    with open('wikipedia.html', 'w', encoding="utf-8") as file:
+    with open('countries.html', 'w', encoding="utf-8") as file:
         file.write(html_doc)
 else:
-    with open('wikipedia.html', encoding="utf-8") as file:
+    with open('countries.html', encoding="utf-8") as file:
         html_doc = file.read()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 table = soup.find('table', class_='wikitable')
 rows = table.find_all('tr')
+
 countries = []
 for row in rows:
-    name_col = row.th
-    name_link = name_col.a
-    if name_link is None:  # Header
+    th = row.th
+    name_link = th.a
+    if not name_link:  # header row
         continue
     name = name_link.string
-    date_of_admission = row.td.text.strip().split('[')[0]
+    date_admitted = row.td.span.string
     country = {
         'Name': name,
-        'Date of admission': date_of_admission
+        'Date admitted': date_admitted,
     }
     countries.append(country)
 
+print(countries)
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, ('Name', 'Date of admission'))
+    writer = csv.DictWriter(file, fieldnames=('Name', 'Date admitted'))
     writer.writeheader()
     writer.writerows(countries)
