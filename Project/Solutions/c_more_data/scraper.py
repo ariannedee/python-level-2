4d39eb5cc398b41c4b743f79aa23b6b19d24cce4
ariannedee <ariannedee@gmail.com>
2024-06-12 12:58:10 -0700

    Update solution for June 2024 class

--- Project/Solutions/c_more_data/scraper.py
+++ Project/Solutions/c_more_data/scraper.py
@@ -2,63 +2,70 @@ import csv
 from time import sleep
 
 import requests
-
 from bs4 import BeautifulSoup
-from pprint import pprint
 
-LOAD_DATA = False
+GET_DATA = False
+
 BASE_URL = "https://en.wikipedia.org"
 URL = BASE_URL + "/wiki/Member_states_of_the_United_Nations"
 
-if LOAD_DATA:
-    response = requests.get(URL)
+name = None
+email = None
+
+headers = {'User-Agent': f'{name} ({email})'}
+
+if GET_DATA:
+    response = requests.get(URL, headers=headers)
     response.raise_for_status()
     html_doc = response.text
-
     with open('countries.html', 'w', encoding="utf-8") as file:
         file.write(html_doc)
 else:
-    with open('countries.html', encoding="utf-8") as file:
+    with open('countries.html', 'r', encoding="utf-8") as file:
         html_doc = file.read()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-
 table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
 
 countries = []
+
+rows = table.find_all('tr')
 for row in rows:
-    th = row.th
-    name_link = th.a
-    if not name_link:  # header row
+    name_col = row.th
+    link = name_col.a
+    if link is None:
         continue
-    name = name_link.string
-    date_admitted = row.td.span.string
+    name = link.string
+    date_joined = row.td.span.string
     country = {
         'Name': name,
-        'Date admitted': date_admitted,
-        'URL': BASE_URL + name_link['href'],
+        'Date joined': date_joined,
+        'URL': BASE_URL + link['href'],
     }
     countries.append(country)
 
-for country in countries[:2]:
-    response = requests.get(country['URL'])
+errors = {}
+
+for country in countries[:3]:
+    url = country['URL']
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(f"Error on {country['URL']}, {response.status_code}: {response.reason}")
+        error = f"Response of {response.status_code} for {url}"
+        errors[country['Name']] = error
         continue
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    try:
-        country['Latitude'] = soup.find('span', class_='latitude').string
-        country['Longitude'] = soup.find('span', class_='longitude').string
-    except Exception as e:
-        print(f"Error getting details for {country['Name']} at {country['URL']}")
-        print(repr(e))
+    latitude = soup.find('span', class_='latitude').string
+    longitude = soup.find('span', class_='longitude').string
+
+    country['Latitude'] = latitude
+    country['Longitude'] = longitude
     sleep(0.5)
 
-pprint(countries)
+print(countries)
+print(len(countries))
 
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=('Name', 'Date admitted', 'Latitude', 'Longitude', 'URL'))
+    writer = csv.DictWriter(file, fieldnames=['Name', 'Date joined', 'Latitude', 'Longitude', 'URL'])
     writer.writeheader()
     writer.writerows(countries)
