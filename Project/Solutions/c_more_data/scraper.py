4c167dbca340a796009fd3fe403dc54b9d5cd020
ariannedee <ariannedee@gmail.com>
2025-02-21 13:01:53 -0800

    Update for Feb 2025 class

--- Project/Solutions/c_more_data/scraper.py
+++ Project/Solutions/c_more_data/scraper.py
@@ -1,71 +1,52 @@
 import csv
-from time import sleep
+import time
 
 import requests
 from bs4 import BeautifulSoup
 
-GET_DATA = False
+BASE_URL = 'https://en.wikipedia.org'
+URL = BASE_URL + '/wiki/Member_states_of_the_United_Nations'
 
-BASE_URL = "https://en.wikipedia.org"
-URL = BASE_URL + "/wiki/Member_states_of_the_United_Nations"
+response = requests.get(URL)
+response.raise_for_status()
 
-name = None
-email = None
-
-headers = {'User-Agent': f'{name} ({email})'}
-
-if GET_DATA:
-    response = requests.get(URL, headers=headers)
-    response.raise_for_status()
-    html_doc = response.text
-    with open('countries.html', 'w', encoding="utf-8") as file:
-        file.write(html_doc)
-else:
-    with open('countries.html', 'r', encoding="utf-8") as file:
-        html_doc = file.read()
+html_doc = response.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
+
 table = soup.find('table', class_='wikitable')
 
 countries = []
 
-rows = table.find_all('tr')
-for row in rows:
-    name_col = row.th
-    link = name_col.a
+for row in table.find_all('tr'):
+    link = row.th.a
     if link is None:
         continue
-    name = link.string
+
+    href = link['href']
+    link_content: str = link.string
+    name = link_content.split(' (')[0]
     date_joined = row.td.span.string
+
     country = {
         'Name': name,
         'Date joined': date_joined,
-        'URL': BASE_URL + link['href'],
+        'URL': href,
     }
     countries.append(country)
 
-errors = {}
-
 for country in countries[:3]:
-    url = country['URL']
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        error = f"Response of {response.status_code} for {url}"
-        errors[country['Name']] = error
-        continue
+    response = requests.get(BASE_URL + country['URL'])
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    latitude = soup.find('span', class_='latitude').string
-    longitude = soup.find('span', class_='longitude').string
 
-    country['Latitude'] = latitude
-    country['Longitude'] = longitude
-    sleep(0.5)
+    country['Latitude'] = soup.find('span', class_='latitude').string
+    country['Longitude'] = soup.find('span', class_='longitude').string
+    time.sleep(0.5)
 
 print(countries)
-print(len(countries))
 
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=['Name', 'Date joined', 'Latitude', 'Longitude', 'URL'])
+    writer = csv.DictWriter(file, ['Name', 'Date joined', 'Latitude', 'Longitude', 'URL'])
     writer.writeheader()
     writer.writerows(countries)
