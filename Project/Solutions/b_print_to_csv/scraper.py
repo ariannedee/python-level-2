890fb662345e60fa79e8b038d1f2db2516aaad2e
ariannedee <ariannedee@gmail.com>
2025-02-21 12:49:59 -0800

    Update for Feb 2025 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -3,38 +3,28 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-GET_DATA = True
+URL = 'https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations'
 
-URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
+response = requests.get(URL)
+response.raise_for_status()
 
-name = None
-email = None
-
-headers = {'User-Agent': f'{name} ({email})'}
-
-if GET_DATA:
-    response = requests.get(URL, headers=headers)
-    response.raise_for_status()
-    html_doc = response.text
-    with open('countries.html', 'w', encoding="utf-8") as file:
-        file.write(html_doc)
-else:
-    with open('countries.html', 'r', encoding="utf-8") as file:
-        html_doc = file.read()
+html_doc = response.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
+
 table = soup.find('table', class_='wikitable')
 
 countries = []
 
-rows = table.find_all('tr')
-for row in rows:
-    name_col = row.th
-    link = name_col.a
-    if link is None:
+for row in table.find_all('tr'):
+    a = row.th.a
+    if a is None:
         continue
-    name = link.string
+
+    link_content: str = a.string
+    name = link_content.split(' (')[0]
     date_joined = row.td.span.string
+
     country = {
         'Name': name,
         'Date joined': date_joined,
@@ -42,9 +32,8 @@ for row in rows:
     countries.append(country)
 
 print(countries)
-print(len(countries))
 
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=['Name', 'Date joined'])
+    writer = csv.DictWriter(file, ['Name', 'Date joined'])
     writer.writeheader()
     writer.writerows(countries)
