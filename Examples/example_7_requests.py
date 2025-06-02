5a4c2ec846f5cfe32c911558e72d9e3a0285f258
ariannedee <ariannedee@gmail.com>
2025-06-02 12:13:16 -0700

    Add params and reason to example

--- Examples/example_7_requests.py
+++ Examples/example_7_requests.py
@@ -3,8 +3,10 @@ import requests
 URL = "https://google.com"
 
 # Dictionary of HTTP headers
-headers = {'User-Agent': f'Your name (your@email.com)'}
-response = requests.get(URL, headers=headers)
+headers = {'User-Agent': f'your@email.com'}  # Metadata for request
+params = {'param1': 123, 'param2': 456}      # Adds ?param1=123&param2=456 to URL
+
+response = requests.get(URL, params=params, headers=headers)
 
 # Full list of HTTP status codes: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 # Common status codes:
@@ -13,6 +15,7 @@ response = requests.get(URL, headers=headers)
 # - 404 Not found
 # - 502 Bad gateway
 print(response.status_code)
+print(response.reason)
 
 # Raise exception if unsuccessful
 response.raise_for_status()
