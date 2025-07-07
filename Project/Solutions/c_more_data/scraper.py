248c5e034cb568ba9166ac570a0a83df3a98f870
ariannedee <ariannedee@gmail.com>
2025-07-07 14:20:56 -0700

    Update for July 2025 class

--- Project/Solutions/c_more_data/scraper.py
+++ Project/Solutions/c_more_data/scraper.py
@@ -1,52 +1,58 @@
 import csv
-import time
 
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://en.wikipedia.org'
-URL = BASE_URL + '/wiki/Member_states_of_the_United_Nations'
+BASE_URL = "https://en.wikipedia.org"
+URL = BASE_URL + "/wiki/Member_states_of_the_United_Nations"
 
 response = requests.get(URL)
 response.raise_for_status()
 
 html_doc = response.text
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')
 
 countries = []
-
-for row in table.find_all('tr'):
-    link = row.th.a
-    if link is None:
+for row in rows:
+    name_col = row.th
+    name_link = name_col.a
+    if name_link is None:  # Ignore header
         continue
-
-    href = link['href']
-    link_content: str = link.string
-    name = link_content.split(' (')[0]
-    date_joined = row.td.span.string
-
+    name = name_link.string
+    admission_date = row.td.span.string
     country = {
         'Name': name,
-        'Date joined': date_joined,
-        'URL': href,
+        'Date of Admission': admission_date,
+        'URL': name_link['href']
     }
     countries.append(country)
 
-for country in countries[:3]:
-    response = requests.get(BASE_URL + country['URL'])
-    html_doc = response.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
+errors = []
+for country in countries:
+    url = BASE_URL + country['URL']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        country['Latitude'] = soup.find('span', class_='latitude').string
+        country['Longitude'] = soup.find('span', class_='longitude').string
+    except Exception as e:
+        error = {
+            'Country': country['Name'],
+            'URL': url,
+            'Error': repr(e),
+        }
+        errors.append(error)
 
-    country['Latitude'] = soup.find('span', class_='latitude').string
-    country['Longitude'] = soup.find('span', class_='longitude').string
-    time.sleep(0.5)
-
-print(countries)
-
+print(errors)
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, ['Name', 'Date joined', 'Latitude', 'Longitude', 'URL'])
+    writer = csv.DictWriter(file, ['Name', 'Date of Admission', 'Latitude', 'Longitude', 'URL'])
     writer.writeheader()
     writer.writerows(countries)
+
+with open('errors.csv', 'w') as file:
+    writer = csv.DictWriter(file, ['Country', 'URL', 'Error'])
+    writer.writeheader()
+    writer.writerows(errors)
