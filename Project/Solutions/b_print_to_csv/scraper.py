a36f24802e270b47bfd7db452d31515d22d993c1
ariannedee <ariannedee@gmail.com>
2025-07-07 13:00:18 -0700

    Update for July 2025 class

--- Project/Solutions/b_print_to_csv/scraper.py
+++ Project/Solutions/b_print_to_csv/scraper.py
@@ -3,37 +3,31 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations'
+URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
 response = requests.get(URL)
 response.raise_for_status()
 
 html_doc = response.text
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 table = soup.find('table', class_='wikitable')
+rows = table.find_all('tr')
 
 countries = []
-
-for row in table.find_all('tr'):
-    a = row.th.a
-    if a is None:
+for row in rows:
+    name_col = row.th
+    name_link = name_col.a
+    if name_link is None:  # Ignore header
         continue
-
-    link_content: str = a.string
-    name = link_content.split(' (')[0]
-    date_joined = row.td.span.string
-
-    country = {
-        'Name': name,
-        'Date joined': date_joined,
-    }
+    name = name_link.string
+    admission_date = row.td.span.string
+    country = {'Name': name, 'Date of Admission': admission_date}
     countries.append(country)
 
 print(countries)
 
 with open('countries.csv', 'w') as file:
-    writer = csv.DictWriter(file, ['Name', 'Date joined'])
+    writer = csv.DictWriter(file, ['Name', 'Date of Admission'])
     writer.writeheader()
     writer.writerows(countries)
