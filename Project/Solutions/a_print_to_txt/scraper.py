db4358cc5a3f58d2ae5d4279e788eab2810e2d59
ariannedee <ariannedee@gmail.com>
2025-07-07 12:55:21 -0700

    Update for July 2025 class

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -5,22 +5,24 @@ URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
 response = requests.get(URL)
 response.raise_for_status()
-html_doc = response.text
 
+html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 table = soup.find('table', class_='wikitable')
 rows = table.find_all('tr')
 
 countries = []
-
 for row in rows:
-    name_link = row.th.a
-    if not name_link:
+    name_col = row.th
+    name_link = name_col.a
+    if name_link is None:  # Ignore header
         continue
+    name = name_link.string
+    countries.append(name)
 
-    name: str = name_link.string
-    countries.append(name.split(' (')[0])
+print(countries)
+print(len(countries))
 
 with open('countries.txt', 'w') as file:
     file.write('\n'.join(countries))
