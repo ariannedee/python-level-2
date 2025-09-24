f235f79d205f1248669e921bb39b74b8b1d505e9
ariannedee <ariannedee@gmail.com>
2025-09-24 12:37:29 -0700

    Update solutions for Sept 2025 class

--- Project/Solutions/a_print_to_txt/scraper.py
+++ Project/Solutions/a_print_to_txt/scraper.py
@@ -3,26 +3,30 @@ from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations"
 
-response = requests.get(URL)
+name = None
+email = None
+
+assert name and email, "Please supply your name and email"
+
+headers = {"User-Agent": f"{name} ({email})"}
+response = requests.get(URL, headers=headers)
+
 response.raise_for_status()
 
 html_doc = response.text
-soup = BeautifulSoup(html_doc, 'html.parser')
 
-table = soup.find('table', class_='wikitable')
-rows = table.find_all('tr')
+soup = BeautifulSoup(html_doc, "html.parser")
+
+table = soup.find("table", class_="wikitable")
 
 countries = []
-for row in rows:
-    name_col = row.th
-    name_link = name_col.a
-    if name_link is None:  # Ignore header
+for row in table.find_all("tr"):
+    name_link = row.th.a
+    if not name_link:
         continue
-    name = name_link.string
+    name: str = name_link.string
+    name = name.split(" (")[0]
     countries.append(name)
 
-print(countries)
-print(len(countries))
-
-with open('countries.txt', 'w') as file:
-    file.write('\n'.join(countries))
+with open("countries.txt", "w") as file:
+    file.write("\n".join(countries))
